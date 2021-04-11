a255486318e7846ecc391dc49539389f74a26267
Yuri Matias <yuri.matias20@gmail.com>
2021-04-11 16:59:01 -0300

    Apenas mudaças de adição de campos no modelo

--- Stop.py
+++ Stop.py
@@ -1,6 +1,6 @@
 import boto3
 region = ''
-instances = ['']
+instances = ['','']
 ec2 = boto3.client('ec2', region_name=region)
 def lambda_handler(event, context):
  ec2.stop_instances(InstanceIds=instances)
